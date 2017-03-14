1ef503065a01442044ebc860cf1d7f78f55e3de6
Kevin Powell <Kevin.Powell@morningstar.com>
2017-03-14 10:51:10 -0500

    Merge pull request #25 in TECHSTD/mds from feature/MDS-212--component-page-template to develop
    Squashed commit of the following:
    
    commit cea7661a599d73e96bc67eb7c079e3962645a59b
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Mon Mar 13 17:17:17 2017 -0400
    
        MDS-212 Update responsive behavior for comonent page
    
    commit 67775be03fc454b49096cda1e0158eed410d0ef5
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Mon Mar 13 14:52:19 2017 -0400
    
        MDS-212 add active icon to active item in site nav
    
    commit 717547c3fcaa7aff6c00059547cacd728223351f
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Mon Mar 13 14:32:27 2017 -0400
    
        MDS-212 add animation to list group sublist items
    
    commit 39eaeb131658711b47c714ad601584932709c4c7
    Merge: 2ee5903 d98d339
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Mon Mar 13 14:25:28 2017 -0400
    
        Reintegrate with develop branch
    
    commit 2ee5903839916cb205bfad7142e0da84840877b5
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Mon Mar 13 14:20:01 2017 -0400
    
        MDS-212 build constants.scss
    
    commit 92372797f1bca52162aabc265c4a60d05ec21db0
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Mon Mar 13 12:54:28 2017 -0400
    
        MDS-212 Update Site Nav to use List Group
    
    commit d08ad6ca2cd772017bb44ad4dc38a1a64af4404f
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Mon Mar 13 12:33:21 2017 -0400
    
        MDS-212 add list group based page nav to component template
    
    commit f24e0e757968689c9a09393b24f3312275974e62
    Merge: d28cce2 b13bb4e
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Mon Mar 13 09:29:48 2017 -0400
    
        Merge branch 'feature/MDS-228--example-code-viewer' into feature/MDS-212--component-page-template
    
    commit b13bb4e770ebb918b355695a9ba3a0c489c6e204
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Mon Mar 13 09:29:01 2017 -0400
    
        Add default label to Buttons
    
    commit d28cce263cb2e097c2336c8e1aa6e03c2a395e1e
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Mon Mar 13 09:22:18 2017 -0400
    
        MDS-212 Update list group to support doc site templates
    
    commit 25cf052b075a6e65686cdcb9ac39341d4c58f304
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Mon Mar 13 08:13:12 2017 -0400
    
        MDS-228: Update z-index for modal and modal-content
    
    commit d3e94f468ac22b7309022abba528e548aa949d51
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Fri Mar 10 16:30:54 2017 -0600
    
        MDS-212 WIP
    
    commit 6cbdacebaa001eb32baa760f00a9d75544f4a5fb
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Fri Mar 10 07:19:17 2017 -0600
    
        MDS-212 WIP
    
    commit 68f80d6b4771f5a4a6f03ac52647d920db87e1c9
    Author: Kevin Powell <kevin@eightshapes.com>
    Date:   Thu Mar 9 15:25:46 2017 -0600
    
        MDS-228 Example Code Viewer

---
+++ src/doc_library/components/page_nav/page_nav.js
@@ -0,0 +1,83 @@
+/*global $*/
+
+var Mds = Mds || {};
+var projectNamespace = projectNamespace || 'mds';
+
+Mds.PageNav = function() {
+    'use strict';
+
+    var buildAnchorNav = function($nav) {
+        const $target = $($nav.attr("data-target")),
+              pageHeaderClass = $nav.attr("data-page-header-class"),
+              $headers = $target.find(" > h2, > h3"),
+              $listGroupTemplate = $nav.find(".mds-doc-page-nav__list-group-template"),
+              $headerTemplate = $nav.find(".mds-doc-page-nav__header-template"),
+              $linkTemplate = $nav.find(".mds-doc-page-nav__link-template"),
+              $listGroupsWrap = $nav.find(".mds-doc-page-nav__list-groups-wrap");
+        var listGroups = [],
+            listGroup = [];
+
+        $headers.each(function(){
+            if ($(this).is('h2') && listGroup.length > 0) {
+                listGroups.push(listGroup);
+                listGroup = [];
+                listGroup.push($(this));
+            } else {
+                listGroup.push($(this));
+            }
+        });
+        listGroups.push(listGroup);
+
+        listGroups.forEach(function(group){
+            const $listGroup = $listGroupTemplate.clone().removeClass("mds-doc-page-nav__list-group-template").empty();
+
+            group.forEach(function(listItem){
+                if ($(listItem).is('h2')) {
+                    const headerText = $(listItem).text(),
+                          $listItem = $headerTemplate.clone().
+                                      removeClass("mds-doc-page-nav__header-template").text(headerText);
+                    $listGroup.append($listItem);
+                } else {
+
+                    const headerText = $(listItem).text(),
+                          headerId = typeof $(listItem).attr("id") === 'undefined' ? headerText.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)/g, '') : $(listItem).attr("id"),
+                          $listItem = $linkTemplate.clone().
+                                        removeClass("mds-doc-page-nav__link-template").
+                                        find(".mds-list-group__item-label").text(headerText).end().
+                                        find(".mds-list-group__link").attr("href", "#" + headerId).end();
+                    $(listItem).attr("id", headerId);
+
+                    if (typeof pageHeaderClass !== 'undefined') {
+                        $(listItem).addClass(pageHeaderClass);
+                    }
+
+                    $listGroup.append($listItem);
+                }
+
+
+            });
+
+            $listGroupsWrap.append($listGroup);
+        });
+
+        $listGroupTemplate.remove();
+    };
+
+    var initialize = function initialize() {
+        $(".mds-doc-page-nav").each(function(){
+            buildAnchorNav($(this));
+        });
+    };
+
+    var public_vars = {
+        'initialize': initialize
+    };
+
+    return public_vars;
+}();
+
+
+$(document).ready(function(){
+    'use strict';
+    Mds.PageNav.initialize();
+});
--- src/doc_library/components/site_nav/site_nav.js
+++ src/doc_library/components/site_nav/site_nav.js
@@ -6,13 +6,17 @@ var projectNamespace = projectNamespace || 'mds';
 Mds.SiteNav = function() {
     'use strict';
     var highlightActiveLink = function() {
+        const $activeIcon = $(".mds-doc-site-nav .mds-list-group__active-icon");
+
         var currentPath = window.location.pathname;
             currentPath = currentPath === '/index.html' ? '/' : currentPath;
 
-        var $activeLink = $('.mds-doc__navigation-link[href~="' + currentPath + '"]'),
-            $parentAccordionTrigger = $activeLink.closest(".mds-doc__navigation-sublist").siblings(".mds-doc__navigation-sublist-toggle-input");
+        var $activeLink = $('.mds-doc-site-nav .mds-list-group__link[href~="' + currentPath + '"]'),
+            $parentAccordionTrigger = $activeLink.closest(".mds-list-group__sublist").siblings(".mds-list-group__sublist-toggle-input");
 
-        $activeLink.addClass("mds-doc__navigation-link--active");
+        // Clear the active state from the first list item
+        $('.mds-doc-site-nav .mds-list-group__item--active').removeClass("mds-list-group__item--active");
+        $activeLink.parent("li").addClass("mds-list-group__item--active").append($activeIcon);
 
         if ($parentAccordionTrigger.length > 0) {
             $parentAccordionTrigger.prop('checked', true);
