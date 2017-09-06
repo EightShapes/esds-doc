0073c4af6be2af1fcf68e1ee0645cbd03a1435a6
Kevin Powell <kevin@eightshapes.com>
2017-09-06 16:50:46 -0500

    WIP: Code Snippet component

---
+++ scripts/global.js
@@ -0,0 +1,19 @@
+'use strict';
+var Esds = Esds || {};
+
+// Polyfill closest() for IE 11 & Edge 14
+if (!Element.prototype.matches)
+    Element.prototype.matches = Element.prototype.msMatchesSelector || 
+                                Element.prototype.webkitMatchesSelector;
+
+if (!Element.prototype.closest)
+    Element.prototype.closest = function(s) {
+        var el = this;
+        var ancestor = this;
+        if (!document.documentElement.contains(el)) return null;
+        do {
+            if (ancestor.matches(s)) return ancestor;
+            ancestor = ancestor.parentElement;
+        } while (ancestor !== null); 
+        return null;
+    };
