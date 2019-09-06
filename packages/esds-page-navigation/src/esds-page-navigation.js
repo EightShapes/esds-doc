7405517ada32994ff39d91cf2ce3c8d3ae5c5050
Kevin Powell <kevin@eightshapes.com>
2019-09-06 16:08:30 -0500

    Add fixed position monitoring

--- packages/esds-page-navigation/src/esds-page-navigation.js
+++ packages/esds-page-navigation/src/esds-page-navigation.js
@@ -1,11 +1,13 @@
 import { LitElement, html } from 'lit-element';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
+import scrollMonitor from 'scrollmonitor';
 
 export class EsdsPageNavigation extends LitElement {
   static get properties() {
     return {
       bottomContent: { type: String, attribute: 'bottom-content' },
       contentSelectors: { type: Array, attribute: 'content-selectors' },
+      fixed: { type: Boolean },
       fixedDistanceFromTop: {
         type: Number,
         attribute: 'fixed-distance-from-top',
@@ -19,8 +21,17 @@ export class EsdsPageNavigation extends LitElement {
   constructor() {
     super();
     this.contentSelectors = ['h2'];
+    this.fixed = false;
+
     if (!this.items) {
       this.updateNavItems();
+    }
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    if (this.fixedDistanceFromTop !== undefined) {
+      this.monitorFixedState(); // TODO: Unbind this in disconnectedCallback
     }
   }
 
@@ -45,12 +56,36 @@ export class EsdsPageNavigation extends LitElement {
     this.selectNavItem(e.target.getAttribute('href'));
   }
 
+  monitorFixedState() {
+    const fixedScrollMonitor = scrollMonitor.create(this, {
+      top: this.fixedDistanceFromTop,
+    });
+
+    console.log(fixedScrollMonitor);
+
+    this.updateFixedState(fixedScrollMonitor);
+    fixedScrollMonitor.stateChange(() => {
+      this.updateFixedState(fixedScrollMonitor);
+    });
+  }
+
   selectNavItem(href) {
     this.items.forEach(i =>
       i.href === href.replace('#', '') ? (i.active = true) : (i.active = false),
     );
 
     this.requestUpdate();
+  }
+
+  updateFixedState(elementWatcher) {
+    console.log('UFS');
+    if (elementWatcher.isAboveViewport && !this.fixed) {
+      this.fixed = true;
+      this.requestUpdate();
+    } else if (!elementWatcher.isAboveViewport && this.fixed) {
+      this.fixed = false;
+      this.requestUpdate();
+    }
   }
 
   updateNavItems() {
@@ -73,29 +108,33 @@ export class EsdsPageNavigation extends LitElement {
 
   render() {
     return html`
-      <nav class="esds-page-navigation"
+      <nav
+        class="esds-page-navigation${this.fixed
+          ? ' esds-page-navigation--fixed'
+          : ''}"
+      >
         <div class="esds-page-navigation__inner">
           ${this.topContent ? unsafeHTML(this.topContent) : ''}
-            <ul class="esds-page-navigation__list">
-              ${this.items.map(i => {
-                let itemClass = 'esds-page-navigation__item';
-                if (i.active) {
-                  itemClass += ' esds-page-navigation__item--active';
-                }
-                return html`
-                  <li class="${itemClass}">
-                    <a
-                      @click=${this.handleNavigationClick}
-                      href="#${i.href}"
-                      class="esds-page-navigation__link"
-                    >
-                      ${unsafeHTML(i.text)}
-                    </a>
-                  </li>
-                `;
-              })}
-            </ul>
-            ${this.bottomContent ? unsafeHTML(this.bottomContent) : ''}
+          <ul class="esds-page-navigation__list">
+            ${this.items.map(i => {
+              let itemClass = 'esds-page-navigation__item';
+              if (i.active) {
+                itemClass += ' esds-page-navigation__item--active';
+              }
+              return html`
+                <li class="${itemClass}">
+                  <a
+                    @click=${this.handleNavigationClick}
+                    href="#${i.href}"
+                    class="esds-page-navigation__link"
+                  >
+                    ${unsafeHTML(i.text)}
+                  </a>
+                </li>
+              `;
+            })}
+          </ul>
+          ${this.bottomContent ? unsafeHTML(this.bottomContent) : ''}
         </div>
       </nav>
     `;
