4a73889755396169bbbc217009b987bf8e972839
Kevin Powell <kevin@eightshapes.com>
2020-03-10 20:22:14 -0500

    Copy storybook architecture from esds-library

---
+++ test/storybook/.storybook/preview.js
@@ -0,0 +1,37 @@
+/* global window */
+import {
+  configure,
+  addParameters,
+  addDecorator,
+  setCustomElements,
+} from '@storybook/web-components';
+
+import { withA11y } from '@storybook/addon-a11y';
+import customElements from '../custom-elements.json';
+import '../stories/styles/annotation-styles.scss';
+import '../stories/styles/hostile-styles.scss';
+
+
+setCustomElements(customElements);
+
+addDecorator(withA11y);
+
+addParameters({
+  docs: {
+    iframeHeight: '200px',
+    inlineStories: false
+  },
+});
+
+// configure(require.context('../stories', true, /\.stories\.(js|mdx)$/), module);
+
+// force full reload to not reregister web components
+const req = require.context('../stories', true, /\.stories\.(js|mdx)$/);
+configure(req, module);
+if (module.hot) {
+  module.hot.accept(req.id, () => {
+    const currentLocationHref = window.location.href;
+    window.history.pushState(null, null, currentLocationHref);
+    window.location.reload();
+  });
+}
